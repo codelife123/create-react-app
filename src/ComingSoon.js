05ec0ec7acafc46e8f1c3a9b0f9ccbd374297d66
codelife123 <nodedevloper@gmail.com>
2022-08-15 15:36:41 +0530

    add coming soon page structure

---
+++ src/ComingSoon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+function ComingSoon() {
+  return (
+    <article>
+      <h1>
+        We are launching soon <span>👏👏</span>
+      </h1>
+      <div>
+        <p>
+          We're creating something awesome 🤩🤩. Please subscribe to get
+          notified when it's ready!
+        </p>
+        <p>&mdash; WhosAlive Team</p>
+      </div>
+    </article>
+  );
+}
+
+export default ComingSoon;
