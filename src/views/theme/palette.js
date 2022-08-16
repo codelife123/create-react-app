731243eb86363876ea490e2208bbfd52044a9aa6
codelife123 <nodedevloper@gmail.com>
2022-08-16 13:08:34 +0530

    remove old libs

--- src/views/theme/palette.js
+++ src/views/theme/palette.js
@@ -1,15 +1,15 @@
 import { light as lightGreen, dark as darkGreen } from './palette--green';
 
 const palette = (themeMode = 'light', paletteType = 'green') => {
-  if (paletteType === 'blue') {
-    return themeMode === 'dark' ? darkBlue : lightBlue;
-  } else if (paletteType === 'indigo') {
-    return themeMode === 'dark' ? darkIndigo : lightIndigo;
-  } else if (paletteType === 'pink') {
-    return themeMode === 'dark' ? darkPink : lightPink;
-  } else if (paletteType === 'orange') {
-    return themeMode === 'dark' ? darkOrange : lightOrange;
-  }
+  // if (paletteType === 'blue') {
+  //   return themeMode === 'dark' ? darkBlue : lightBlue;
+  // } else if (paletteType === 'indigo') {
+  //   return themeMode === 'dark' ? darkIndigo : lightIndigo;
+  // } else if (paletteType === 'pink') {
+  //   return themeMode === 'dark' ? darkPink : lightPink;
+  // } else if (paletteType === 'orange') {
+  //   return themeMode === 'dark' ? darkOrange : lightOrange;
+  // }
 
   return themeMode === 'dark' ? darkGreen : darkGreen;
 };
